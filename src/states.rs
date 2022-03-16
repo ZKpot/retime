41da80c365b60b38cbb6056bd0a04d1472495464
Nikita Zemtsov <z-nik@ya.ru>
2022-03-16 22:24:09 +0100

    Design closed level (#10)

--- src/states.rs
+++ src/states.rs
@@ -64,7 +64,7 @@ pub fn update (
     )>();
 
     for (transform, _) in query {
-        if transform.translate.x > 310.0 {
+        if transform.translate.x > 97.0 {
             stats.level_passed = true;
             state.push(Pause::default());
         }
